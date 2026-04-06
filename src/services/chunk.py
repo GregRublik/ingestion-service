4971df7bdd07650fde6b9ecb9e3b6c8e27abff17
Иванов Григорий <grigorashekivanov2@gmail.com>
2026-04-06 09:19:30 +0300

    add chunk service

--- src/services/chunk.py
+++ src/services/chunk.py
@@ -1,26 +1,50 @@
+from aiobotocore.response import StreamingBody
+from langchain_experimental.text_splitter import SemanticChunker
+
 from models.document import Document
+from repositories.aws import AWSRepository
+from typing import Literal
+
+from langchain_text_splitters import (
+    RecursiveCharacterTextSplitter,
+    CharacterTextSplitter,
+    MarkdownHeaderTextSplitter,
+)
+from config import settings
 
 
 class ChunkingService:
 
-    def chunk(self, doc: Document, payload):
+    def __init__(
+            self,
+        aws_repository: AWSRepository,
+    ):
+        self.aws_repository = aws_repository
 
-        if doc.file_extension == '.docx':
-            pass
-        elif doc.file_extension == '.doc':
-            pass
-        elif doc.file_extension == '.pdf':
-            pass
-        elif doc.file_extension == '.json':
-            pass
-        elif doc.file_extension == '.md':
-            pass
-        elif doc.file_extension == '.txt':
-            pass
-        elif doc.file_extension in ['.jpg', '.png']:
-            pass
+    chunking_methods = {
+        "recursive": RecursiveCharacterTextSplitter,
+        "char": CharacterTextSplitter,
+        "markdown": MarkdownHeaderTextSplitter,
+        "semantic": SemanticChunker
+    }
 
+    def _get_splitter(self, chunk_type: str, params: dict):
+        return self.chunking_methods[chunk_type](**params)
 
+    async def chunk(
+            self,
+            doc: Document,
+            chunk_type: Literal["recursive", "char", "markdown", "syntax"],
+            params: dict
+    ) :
+        file_obj = await self.aws_repository.get_document(
+            settings.aws.bucket_name, doc.file_name
+        )
 
+        splitter = self._get_splitter(chunk_type, params)
 
+        content: StreamingBody = file_obj["body"]
+        cont = await content.read()
+
+        return splitter.split_text(cont)
 
