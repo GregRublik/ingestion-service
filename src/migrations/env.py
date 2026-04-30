b291897a09f0b5e7ad9ed2f7d35b1b5604dfe1c6
Иванов Григорий <grigorashekivanov2@gmail.com>
2026-04-30 16:07:27 +0300

    refresh migrations

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -30,7 +30,7 @@ def run_migrations_offline() -> None:
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
-        version_table="alembic_version_ingestion"
+        # version_table="alembic_version_ingestion"
     )
 
     with context.begin_transaction():
@@ -49,7 +49,7 @@ def run_migrations_online() -> None:
         context.configure(
             connection=connection,
             target_metadata=target_metadata,
-            version_table="alembic_version_ingestion"
+            # version_table="alembic_version_ingestion"
         )
 
         with context.begin_transaction():
