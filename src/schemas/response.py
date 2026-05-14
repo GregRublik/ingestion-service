c79478efa11f01a6a92f12ac62338d5e99e453f2
Иванов Григорий <grigorashekivanov2@gmail.com>
2026-05-14 10:07:23 +0300

    add date to responses

--- src/schemas/response.py
+++ src/schemas/response.py
@@ -1,10 +1,12 @@
 from typing import Generic, TypeVar, Optional
 from pydantic.generics import GenericModel
+from datetime import datetime
 
 T = TypeVar("T")
 
 
 class APIResponse(GenericModel, Generic[T]):
     success: bool = True
+    date: datetime = datetime.now()
     data: Optional[T] = None
     error: Optional[str] = None
